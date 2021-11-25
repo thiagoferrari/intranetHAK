f77f9806c5057e89fea2100e843d84bad52a8aff
Thiago Ferrari <thiagofcjr@gmail.com>
2021-11-24 23:41:32 -0300

    updates

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,7 +4,7 @@ import { extname, resolve } from 'path';
 
 export default {
 	storage: multer.diskStorage({
-		destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+		destination: resolve(__dirname, '..', '..', 'data', 'uploads'),
 		filename: (req, file, cb) => {
 			crypto.randomBytes(16, (err, res) => {
 				if (err) return cb(err);
